a5a531020d65f376eace246390ea7187181a93e6
Kevin Lynch <klynch@gmail.com>
2010-08-28 18:29:49 -0400

    config org-mode

---
+++ config-org.el
@@ -0,0 +1,19 @@
+(require 'cl)
+
+(require 'org-install)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+
+;;org-indent-mode can crash Emacs 23.1, so org-mode refuses to enabled
+(setq org-startup-indented t)
+
+;;org-M-RET-may-split-line
+
+;;;;Sparse Trees
+;;org-show-hierarchy-above
+;;org-show-following-heading
+;;org-show-siblings
+;;org-show-entry-below
+
+(provide 'config-org)
