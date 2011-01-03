965ea650313cdd81332f406a187686d85c315342
Kevin Lynch <klynch@gmail.com>
2011-01-03 17:46:45 -0500

    need to reevaluate python stuff too

--- config-python.el
+++ config-python.el
@@ -2,49 +2,63 @@
 ;;; Python Mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'cl)
+(eval-when-compile (require 'cl))
 
-(autoload 'rst-mode "rst-mode" "RST Mode." t)
-(add-to-list 'auto-mode-alist '("\\.rst$"  . rst-mode))
-(add-to-list 'auto-mode-alist '("\\.rest$" . rst-mode))
-
-(autoload 'doctest-mode "doctest-mode" "doctest mode" t)
-(add-to-list 'auto-mode-alist '("\\.doctest$" . doctest-mode))
-
-(eval-when-compile (require 'python-mode))
-(autoload 'python-mode "python-mode" "Python Mode." t)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-
+;;; Electric Pairs
 (add-hook 'python-mode-hook
           (lambda ()
-            (set-variable 'py-indent-offset 4)
-            ;; (set-variable 'py-smart-indentation nil)
-            (set-variable 'indent-tabs-mode nil)
-            (define-key py-mode-map (kbd "RET") 'newline-and-indent)
-            (define-key py-mode-map [f8]        'flymake-mode)
-            (define-key py-mode-map [f9]        'python-mode)
-            (define-key py-mode-map [f10]       'doctest-mode)
-            (define-key py-mode-map [f11]       'rst-mode)
-            ;; (define-key py-mode-map [tab] 'yas/expand)
-            ;; (setq yas/after-exit-snippet-hook 'indent-according-to-mode)
-            ;; (smart-operator-mode-on)
-            ))
+            (define-key python-mode-map "\"" 'electric-pair)
+            (define-key python-mode-map "\'" 'electric-pair)
+            (define-key python-mode-map "(" 'electric-pair)
+            (define-key python-mode-map "[" 'electric-pair)
+            (define-key python-mode-map "{" 'electric-pair)
+            (define-key python-mode-map "\C-m" 'newline-and-indent)
+            (setq indent-tabs-mode nil)
+            (setq tab-width 4)))
 
-(eval-when-compile (require 'flymake))
-;; code checking via flymake
-;; set code checker here from "epylint", "pyflakes"
-(defvar pycodechecker "pyflakes")
-(when (load "flymake" t)
-  (defun flymake-pycodecheck-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list pycodechecker (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pycodecheck-init)))
+
+
+;;(autoload 'rst-mode "rst-mode" "RST Mode." t)
+;;(add-to-list 'auto-mode-alist '("\\.rst$"  . rst-mode))
+;;(add-to-list 'auto-mode-alist '("\\.rest$" . rst-mode))
+
+;;(autoload 'doctest-mode "doctest-mode" "doctest mode" t)
+;;(add-to-list 'auto-mode-alist '("\\.doctest$" . doctest-mode))
+
+;;(eval-when-compile (require 'python-mode))
+;;(autoload 'python-mode "python-mode" "Python Mode." t)
+;;(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+;;(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
+;; (add-hook 'python-mode-hook
+;;           (lambda ()
+;;             (set-variable 'py-indent-offset 4)
+;;             ;; (set-variable 'py-smart-indentation nil)
+;;             (set-variable 'indent-tabs-mode nil)
+;;             (define-key py-mode-map (kbd "RET") 'newline-and-indent)
+;;             (define-key py-mode-map [f8]        'flymake-mode)
+;;             (define-key py-mode-map [f9]        'python-mode)
+;;             (define-key py-mode-map [f10]       'doctest-mode)
+;;             (define-key py-mode-map [f11]       'rst-mode)
+;;             ;; (define-key py-mode-map [tab] 'yas/expand)
+;;             ;; (setq yas/after-exit-snippet-hook 'indent-according-to-mode)
+;;             ;; (smart-operator-mode-on)
+;;             ))
+
+;; (eval-when-compile (require 'flymake))
+;; ;; code checking via flymake
+;; ;; set code checker here from "epylint", "pyflakes"
+;; (defvar pycodechecker "pyflakes")
+;; (when (load "flymake" t)
+;;   (defun flymake-pycodecheck-init ()
+;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;            (local-file (file-relative-name
+;;                         temp-file
+;;                         (file-name-directory buffer-file-name))))
+;;       (list pycodechecker (list local-file))))
+;;   (add-to-list 'flymake-allowed-file-name-masks
+;;                '("\\.py\\'" flymake-pycodecheck-init)))
 
 ;; (add-hook 'find-file-hook 'flymake-find-file-hook)
 
