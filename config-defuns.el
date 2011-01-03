ee76d664d30d747332774693c88588251f7f6360
Kevin Lynch <klynch@gmail.com>
2011-01-03 17:43:18 -0500

    added new electric-pair, but i'm not really sure how this is better

--- config-defuns.el
+++ config-defuns.el
@@ -46,11 +46,18 @@
   (byte-recompile-directory elisp-dir 0))
 
 (defun electric-pair ()
-  "If at end of line, insert character pair without surrounding spaces.
-   Otherwise, just insert the typed character."
+  "Insert character pair without sournding spaces"
   (interactive)
-  (if (eolp) (let (parens-require-spaces) (insert-pair))
-    (self-insert-command 1)))
+  (let (parens-require-spaces)
+    (insert-pair)))
+
+
+;; (defun electric-pair ()
+;;   "If at end of line, insert character pair without surrounding spaces.
+;;    Otherwise, just insert the typed character."
+;;   (interactive)
+;;   (if (eolp) (let (parens-require-spaces) (insert-pair))
+;;     (self-insert-command 1)))
 
 (defun indent-page ()
   "Indent the entire buffer."
