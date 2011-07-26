d2a07c56500363e132089b40ebfb92743cdcadb0
Kevin Lynch <klynch@gmail.com>
2011-07-26 17:46:37 -0400

    completion is working\!

--- conf-available/conf-complete.el
+++ conf-available/conf-complete.el
@@ -3,17 +3,35 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; YaSnippet
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defvar snippets-dir (expand-dotemacs "snippets")
-  "The directory where all of our snippets are stored")
 
+(when (require 'yasnippet nil 'noerror)
+  (defvar snippets-dir (expand-dotemacs "snippets")
+    "The directory where all of our snippets are stored")
+
+  ;; (setq yas/snippet-dirs (list (expand-file-name "master" snippets-dir)
+  ;;                              (expand-file-name "licenses" snippets-dir)))
+
+  (setq yas/snippet-dirs (expand-file-name "master" snippets-dir))
+
+  (setq yas/prompt-functions '(yas/dropdown-prompt
+                               yas/x-prompt
+                               yas/ido-prompt
+                               yas/completing-prompt))
+
+  ;; (setq yas/wrap-around-region 'cua)
+
+  (yas/load-directory yas/snippet-dirs)
+  (yas/initialize)
+
+  (add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
+  (add-to-list 'auto-mode-alist '(".yas-setup$"   . emacs-lisp-mode))
+
+  (defun yas/load-django-snippets ()'
+    "Load django snippets"
+    (interactive)
+    (yas/load-directory (expand-file-name "django" snippets-dir)))
+  )
 (require 'yasnippet)
-
-(setq yas/snippet-dirs (expand-file-name "master" snippets-dir))
-
-(yas/load-directory yas/snippet-dirs)
-(yas/initialize)
-
-;;(add-to-list 'auto-mode-alist '(snippets-dir . snippet-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Auto-Complete Mode
@@ -23,10 +41,76 @@
 (when (require 'auto-complete-config nil 'noerror)
 
   (add-to-list 'ac-dictionary-directories
-               (expand-file-name-dotemacs "elisp/auto-complete/dict"))
-  (setq ac-comphist-file
-        (expand-file-name-dotemacs "ac-comphist.dat"))
-  (ac-config-default))
+               (file-name-as-directory elisp-dir)
+               (expand-dotemacs "elisp/auto-complete/dict"))
+  (setq ac-comphist-file (expand-dotemacs "ac-comphist.dat"))
+  (setq ac-dwim t)
+  (ac-config-default)
+
+  (ac-set-trigger-key "TAB")
+  (setq ac-auto-start nil)
+
+
+  ;; set also the completion for eshell
+  (add-hook 'eshell-mode-hook 'ac-eshell-mode-setup)
+
+  ;; custom keybindings to use tab, enter and up and down arrows
+  (define-key ac-complete-mode-map "\t" 'ac-expand)
+  (define-key ac-complete-mode-map "\r" 'ac-complete)
+  (define-key ac-complete-mode-map "\M-n" 'ac-next)
+  (define-key ac-complete-mode-map "\M-p" 'ac-previous)
+
+  (message "auto-complete-config!!")
+
+  ;; ;; Disabling Yasnippet completion
+  ;; (defun epy-snips-from-table (table)
+  ;;   (with-no-warnings
+  ;;     (let ((hashtab (ac-yasnippet-table-hash table))
+  ;;           (parent (ac-yasnippet-table-parent table))
+  ;;           candidates)
+  ;;       (maphash (lambda (key value)
+  ;;                  (push key candidates))
+  ;;                hashtab)
+  ;;       (identity candidates)
+  ;;       )))
+
+  ;; (defun epy-get-all-snips ()
+  ;;   (require 'yasnippet) ;; FIXME: find a way to conditionally load it
+  ;;   (let (candidates)
+  ;;     (maphash
+  ;;      (lambda (kk vv) (push (epy-snips-from-table vv) candidates)) yas/tables)
+  ;;     (apply 'append candidates))
+  ;;   )
+
+
+  ;; ;;(setq ac-ignores (concatenate 'list ac-ignores (epy-get-all-snips)))
+
+  ;; ;; ropemacs Integration with auto-completion
+  ;; (defun ac-ropemacs-candidates ()
+  ;;   (mapcar (lambda (completion)
+  ;;             (concat ac-prefix completion))
+  ;;           (rope-completions)))
+
+  ;; (ac-define-source nropemacs
+  ;;   '((candidates . ac-ropemacs-candidates)
+  ;;     (symbol     . "p")))
+
+  ;; (ac-define-source nropemacs-dot
+  ;;   '((candidates . ac-ropemacs-candidates)
+  ;;     (symbol     . "p")
+  ;;     (prefix     . c-dot)
+  ;;     (requires   . 0)))
+
+  ;; (defun ac-nropemacs-setup ()
+  ;;   (setq ac-sources (append '(ac-source-nropemacs
+  ;;                              ac-source-nropemacs-dot) ac-sources)))
+  ;; (defun ac-python-mode-setup ()
+  ;;   (add-to-list 'ac-sources 'ac-source-yasnippet))
+
+  ;; (add-hook 'python-mode-hook 'ac-python-mode-setup)
+  ;; (add-hook 'rope-open-project-hook 'ac-nropemacs-setup)
+
+  )
 (require 'auto-complete-config)
 
 ;; (add-hook 'c++-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-semantic)))
@@ -40,6 +124,9 @@
 ;;           (lambda ()
 ;;             (local-set-key (kbd "C-c .") 'ac-complete-semantic)))
 
-;; (setq ac-dwim t)
+
+;; Eproject project management with emacs
+(require 'eproject)
+
 
 (provide 'conf-complete)
